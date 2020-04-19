e3fd44a61fbd6463841ce07ef59563cc68df4ace
Admin <guozengjie110@foxmail.com>
2020-04-19 18:01:17 +0800

    FFmpegPlayer

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,6 +14,7 @@ JNIEXPORT void JNICALL
 Java_club_guozengjie_cplusplus_FFmpegPlayer_jniPrepare(JNIEnv *env, jobject thiz, jstring jurl) {
     AVFormatContext *ps;
     const char * url=env->GetStringUTFChars(jurl, nullptr);
-    avformat_open_input(&ps,url,nullptr, nullptr);
+    int res=avformat_open_input(&ps,url,nullptr, nullptr);
+    __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", res);
     env->ReleaseStringUTFChars(jurl, url);
 }
