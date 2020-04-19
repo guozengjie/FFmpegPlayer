a648c2af420b5cb3e945e9e40b2edd1557b54f9d
Admin <guozengjie110@foxmail.com>
2020-04-20 00:56:28 +0800

    FFmpegPlayer

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -13,10 +13,13 @@ using namespace std;
 pthread_t pid;
 
 void *task_open_input(void *url) {
-    __android_log_print(ANDROID_LOG_ERROR, "TAG", "直播源地址%s", url);
+//    防止url指向的地址内存释放
+    char *dataSource = new char[100];
+    strcpy(dataSource, static_cast<const char *>(url));
+    __android_log_print(ANDROID_LOG_ERROR, "TAG", "直播源地址%s", dataSource);
     //  打开视频输入流
     AVFormatContext *ps;
-    int res = avformat_open_input(&ps, static_cast<const char *>(url),
+    int res = avformat_open_input(&ps, dataSource,
                                   nullptr, nullptr);
     if (res == 0) {
         __android_log_print(ANDROID_LOG_ERROR, "TAG", "打开直播源成功");
@@ -32,6 +35,6 @@ Java_club_guozengjie_cplusplus_FFmpegPlayer_jniPrepare(JNIEnv *env, jobject thiz
 //    新建线程
     const char *url = env->GetStringUTFChars(jurl, nullptr);
     pthread_create(&pid, nullptr, task_open_input,
-                   (void *) "http://cbnlive.cbchot.com/live/yt_chc_h_1.m3u8?123");
+                   (void *) url);
     env->ReleaseStringUTFChars(jurl, url);
 }
