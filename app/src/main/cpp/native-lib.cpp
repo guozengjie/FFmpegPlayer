13dc632073590cf72b4129c0ac8645da47274239
Admin <guozengjie110@foxmail.com>
2020-04-19 22:46:11 +0800

    FFmpegPlayer

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <android/log.h>
+#include <pthread.h>
 
 
 extern "C" {
@@ -9,12 +10,23 @@ extern "C" {
 }
 using namespace std;
 
+pthread_t pid;
+
+void *run(void *) {
+    __android_log_print(ANDROID_LOG_ERROR, "TAG", "新建线程，打开直播源");
+    //  打开视频输入流
+    AVFormatContext *ps;
+    int res = avformat_open_input(&ps, "http://cbnlive.cbchot.com/live/yt_chc_h_1.m3u8?123",
+                                  nullptr, nullptr);
+    __android_log_print(ANDROID_LOG_ERROR, "TAG", "打开直播源return:%d", res);
+    pthread_exit(&pid);
+}
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_club_guozengjie_cplusplus_FFmpegPlayer_jniPrepare(JNIEnv *env, jobject thiz, jstring jurl) {
-    AVFormatContext *ps;
-    const char * url=env->GetStringUTFChars(jurl, nullptr);
-    int res=avformat_open_input(&ps,url,nullptr, nullptr);
-    __android_log_print(ANDROID_LOG_ERROR, "TAG", "%d", res);
+//    新建线程
+    const char *url = env->GetStringUTFChars(jurl, nullptr);
+    pthread_create(&pid, nullptr, run, nullptr);
     env->ReleaseStringUTFChars(jurl, url);
 }
