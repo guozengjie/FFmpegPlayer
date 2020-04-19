b00c2134e39551cb9bf5bbc587da4f2cbd9a732a
Admin <guozengjie110@foxmail.com>
2020-04-19 23:08:25 +0800

    FFmpegPlayer

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -12,13 +12,17 @@ using namespace std;
 
 pthread_t pid;
 
-void *run(void *) {
-    __android_log_print(ANDROID_LOG_ERROR, "TAG", "新建线程，打开直播源");
+void *task_open_input(void *url) {
+    __android_log_print(ANDROID_LOG_ERROR, "TAG", "直播源地址%s", url);
     //  打开视频输入流
     AVFormatContext *ps;
-    int res = avformat_open_input(&ps, "http://cbnlive.cbchot.com/live/yt_chc_h_1.m3u8?123",
+    int res = avformat_open_input(&ps, static_cast<const char *>(url),
                                   nullptr, nullptr);
-    __android_log_print(ANDROID_LOG_ERROR, "TAG", "打开直播源return:%d", res);
+    if (res == 0) {
+        __android_log_print(ANDROID_LOG_ERROR, "TAG", "打开直播源成功");
+    } else {
+        __android_log_print(ANDROID_LOG_ERROR, "TAG", "打开直播源失败");
+    }
     pthread_exit(&pid);
 }
 
@@ -27,6 +31,7 @@ JNIEXPORT void JNICALL
 Java_club_guozengjie_cplusplus_FFmpegPlayer_jniPrepare(JNIEnv *env, jobject thiz, jstring jurl) {
 //    新建线程
     const char *url = env->GetStringUTFChars(jurl, nullptr);
-    pthread_create(&pid, nullptr, run, nullptr);
+    pthread_create(&pid, nullptr, task_open_input,
+                   (void *) "http://cbnlive.cbchot.com/live/yt_chc_h_1.m3u8?123");
     env->ReleaseStringUTFChars(jurl, url);
 }
