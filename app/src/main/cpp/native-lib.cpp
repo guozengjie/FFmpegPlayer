1bacdd7f0258769169b7f3ea71076391f799eb69
Admin <guozengjie110@foxmail.com>
2020-04-20 18:49:26 +0800

    FFmpegPlayer

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -9,22 +9,22 @@ extern "C" {
 #include <libavformat/avformat.h>
 }
 using namespace std;
-
+#define loge(...) __android_log_print(ANDROID_LOG_ERROR, "FFmpeg", __VA_ARGS__)
 pthread_t pid;
 
 void *task_open_input(void *url) {
 //    防止url指向的地址内存释放
     char *dataSource = new char[256];
     strcpy(dataSource, static_cast<const char *>(url));
-    __android_log_print(ANDROID_LOG_ERROR, "TAG", "直播源地址%s", dataSource);
+    loge("直播源地址%s", dataSource);
     //  打开视频输入流
     AVFormatContext *ps;
     int res = avformat_open_input(&ps, dataSource,
                                   nullptr, nullptr);
     if (res == 0) {
-        __android_log_print(ANDROID_LOG_ERROR, "TAG", "打开直播源成功");
+        loge("打开直播源成功");
     } else {
-        __android_log_print(ANDROID_LOG_ERROR, "TAG", "打开直播源失败");
+        loge("打开直播源失败");
     }
     pthread_exit(&pid);
 }
